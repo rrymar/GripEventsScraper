3b0b8b290d81a7e198e55fc6b59b68c00257cd6d
RuslanRymar <rrimar@idiq.com>
2023-04-21 17:14:15 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GripEventsScraper
+Scrape Visitors from grip.events
+
+Replace variables to use:
+- visitorListUrl
+- token
+
+
+Copy Script into Browser console and run.
---
+++ ScrapePersons.js
@@ -0,0 +1,36 @@
+﻿// Visitor List Url with our page number
+const visitorListUrl = 'https://api-prod.grip.events/1/container/5389/search?search=&sort=name&order=asc&type_id=1866,1933,1949,1884&page=';
+const token ='your token - can be obtain in network tab';
+
+const maxPage = 200;
+let results = new Array();
+
+let page = 1;
+while(true){
+    
+    const response = await fetch(visitorListUrl + page, {
+        headers: {
+            "x-authorization": token
+        }
+    });
+
+    console.log(`Page: ${page} loaded`);
+
+    const json = await response.json();
+
+    if (json.success)
+        results = results.concat(json.data);
+
+    if(!json.success || json.data.length == 0 || page > maxPage)
+        break;
+    
+    page++;
+} 
+
+console.log(results.length);
+
+ for (const index in results) {
+     const item = results[index];
+     console.log(`${item.name} -- ${item.job_title} in ${item.company_name} (${item.location})`);
+ }
+
